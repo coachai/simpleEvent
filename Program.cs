c0f71b6bd16d76b802f44af9aa0b0dad8c72d25e
HUa <huachai2011@hotmail.com>
2016-11-10 11:47:19 +0100

    Event

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simpleEvent
+{   class Observable
+    {
+        public event EventHandler SomethingHappened;
+        public void DoSomething()
+        { EventHandler handler = SomethingHappened;
+            if (handler != null)
+            { handler(this, EventArgs.Empty);
+              
+            }
+        }
+    }
+
+    class Observer
+    {
+        public void HandleEvent(object sender, EventArgs args)
+        { Debug.WriteLine("Something happened to " + sender); }
+    }
+
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            Observable observable = new Observable();
+            Observer observer = new Observer();
+            observable.SomethingHappened += observer.HandleEvent;
+
+            observable.DoSomething();
+        }
+    }
+}
